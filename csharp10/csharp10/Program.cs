f82874984670d583a15d9eabf4ea052114941df1
gled4er <kadimitr@microsoft.com>
2021-11-04 15:46:18 +0000

    Adding initial struct sample

--- csharp10/csharp10/Program.cs
+++ csharp10/csharp10/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using csharp10.Struct;
+
+var m1 = new Measurement();
+Console.WriteLine(m1);
+
+var m2 = default(Measurement);
+Console.WriteLine(m2);
+
+var ms = new Measurement[2];
+Console.WriteLine(string.Join(",", ms));
+
---
+++ csharp10/csharp10/struct/Measurement.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace csharp10.Struct
+{
+    internal struct Measurement
+    {
+        public Measurement()
+        {
+            Value = double.NaN;
+            Description = "Undefined";
+        }
+
+        internal Measurement(double value, string description)
+        {
+            Value = value;
+            Description = description;
+        }
+
+
+        public double Value { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString() => $"{Value} ({Description})";
+    }
+}
